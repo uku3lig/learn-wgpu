ddd2d4a581ee10199f3b7bc7d303f6d7ea73932b
uku <uku3lig@gmail.com>
2023-05-30 09:05:50 +0200

    challenge: surface

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     window: Window,
+    clear_color: wgpu::Color,
 }
 
 impl State {
@@ -63,6 +64,8 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        let clear_color = wgpu::Color::BLACK;
+
         Self {
             window,
             surface,
@@ -70,6 +73,7 @@ impl State {
             queue,
             config,
             size,
+            clear_color,
         }
     }
 
@@ -87,7 +91,19 @@ impl State {
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        false
+        match event {
+            WindowEvent::CursorMoved { position, .. } => {
+                self.clear_color = wgpu::Color {
+                    r: position.x / self.size.width as f64,
+                    g: position.y / self.size.height as f64,
+                    b: 0.5,
+                    a: 1.0,
+                };
+
+                true
+            }
+            _ => false,
+        }
     }
 
     fn update(&mut self) {}
@@ -110,12 +126,7 @@ impl State {
                     view: &view,
                     resolve_target: None,
                     ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
-                        }),
+                        load: wgpu::LoadOp::Clear(self.clear_color),
                         store: true,
                     },
                 })],
